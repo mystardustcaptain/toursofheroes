47a5143d8a4a8c16274fb59704a12144bcd96f28
Captain Stardust <stand.by.matt@gmail.com>
2018-09-15 13:11:58 +0800

    Master/Detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'toursofheroes';
+  title = 'Tours of Heroes';
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero[] = [
+    { id: 11, name: "Hair" },
+    { id: 12, name: "Eyes" },
+    { id: 13, name: "Ears" },
+    { id: 14, name: "Nose" },
+    { id: 15, name: "Mouth" }
+]
